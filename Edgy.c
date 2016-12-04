950f819170f70f51200794a723e611a01fbad9bf
Thant Htoo Zaw <htoo97@users.noreply.github.com>
2016-12-04 01:33:40 -0800

    implemented led array and buzzer functionality

--- Edgy.c
+++ Edgy.c
@@ -1,6 +1,56 @@
 #include <Servo.h>
 #include "Edgy.h"
 
+
+
+
+
+
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+static const uint8_t PROGMEM
+  arrow[] =
+  { B00011000,
+    B00011000,
+    B00011000,
+    B10011001,
+    B11011011,
+    B01111110,
+    B00111100,
+    B00011000 },
+
+
+
+
+  sadface[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10011001,
+    B10100101,
+    B01000010,
+    B00111100 },
+  
+  smilyface[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10100101,
+    B10011001,
+    B01000010,
+    B00111100 };
+
+
+
+
+
+
+
+
+Song song(buzzer);
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -12,6 +62,7 @@ void setup()
   button.setup();
   bump.setup();
   bump_3.setup();
+  matrix.begin(0x84);
   
 }
 // state definitions
@@ -23,49 +74,139 @@ const int R = 4; // spin right
 const int FF = 5; // fast forward + angry face
 const int HUG = 6; // close up to the object, hug, smile, release gripper
 
+
 // initial state
 int state = SF;
+
 
 // flags indicating if there are walls on either side of vehicle
 bool leftWall = false;
 bool rightWall = false;
 
+
 // signal to charge ahead
 bool charge = false; 
 
-void loop() {
 
-  switch(state) {
+void loop()
+{
+buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_G4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+
+
+    // Measure 2
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_B4, 250);
+    delay(500);        // We delay a little longer here to emulate the half note
+
+
+    // Measure 3
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 500);
+    delay(500);        // We delay a little longer here to emulate the half note
+
+
+    // Measure 4
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_D5, 250);
+    delay(250);
+    buzzer.playNote(NOTE_D5, 500);
+    delay(500);        // We delay a little longer here to emulate the half note
+
+
+    // Measure 5
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_G4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+
+
+    // Measure 6
+    
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_B4, 500);
+    delay(500);        // We delay a little longer here to emulate the half note
+
+
+    // Measure 7
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_B4, 250);
+    delay(250);
+    buzzer.playNote(NOTE_A4, 250);
+    delay(250);
+
+
+    // Measure 8
+    buzzer.playNote(NOTE_G4, 1000);
+    delay(1000);
+
+
+switch(state) {
+
+
    case S:
       drive.stop();
       delay(500); // 0.5 second
       
       if (charge) {
-        state = HUG;
+        charge = false;
+        state = HUG;   
       }
       else {
         state = L;
       }
       break;
 
+
     case SF:
-      drive.forward(128); // half-speed
-      // TODO: add random shape: forward arrow, sun or sth that indicates serenity/calmness
+      matrix.clear();
+      drive.backward(128); // half-speed
+      
+      display.clear();
+      display.drawBitmap(0, 0, arrow, 8, 8, LED_ON);
+      display.writeDisplay();
+      
       if (distanceSensor.get_distance() < 100) {
         state = S;
       }
       break;
 
+
     case B:
-      drive.backward(128);
+      
+      drive.forward(128);
       delay(1000);
-      drive.spinRight(128);
+      drive.spinLeft(128);
       delay(600);
       state = SF; // restart
       break;
 
+
     case L:
-      drive.spinLeft(128);
+      drive.spinRight(128);
       delay(200);
       if (distanceSensor.get_distance() < 100) {
         leftWall = true; 
@@ -73,8 +214,9 @@ void loop() {
       state = R;
       break;
 
+
     case R:
-      drive.spinRight(128);
+      drive.spinLeft(128);
       delay(400);
       if (distanceSensor.get_distance() < 100) {
         rightWall = true; 
@@ -82,109 +224,54 @@ void loop() {
       
       // distance detected is a wall
       if (leftWall & rightWall) {
-        spinRight(128);
-        delay(400);
+        drive.spinRight(128);
+        delay(200);
         leftWall = false;
         rightWall = false;
         state = SF;
       }
       else {
         charge = true;
-        drive.spinLeft(128);
+        drive.spinRight(128);
         delay(200);
-        state = FF:
+        state = FF;
       }
       break;
 
+
     case FF:
-      drive.forward(); // full speed
-      // TODO: add angry face >_<
-      if (distanceSensor.get_distance() < 20) {
+      drive.backward(); // full speed
+      
+      display.clear();
+      display.drawBitmap(0,0,sadface,8,8,LED_ON);
+      display.writeDisplay();
+      
+      if (distanceSensor.get_distance() < 30) {
          state = S;
       }
       break;
 
+
     case HUG:
-      pincer.open();
-      delay(200);
+        pincer.open();
+        delay(2000);
    
-      drive.forward(128); 
+      drive.backward(128); 
       delay(600); // to fine-tune so that object is in pincher's grab
       
       pincer.close();
-      // TODO: add smiling face :)     
       delay(2000);
       
-      pincer.open();
+      display.clear();
+      display.drawBitmap(0, 0, smilyface, 8, 8, LED_ON);
+      display.writeDisplay();    
+      delay(2000);
+      
+      pincer.open(); 
+      delay(500);
       state = B;
       break;
   } // end of switch case
 
-        /**********************
-         ORIGINAL TESTER CODE
-        ***********************/
-//  Serial.println("Testing pincer...");
-//  	for(int i = 0; i < 5; i++) {
-//  	pincer.open();
-//  	delay(1500);
-//  	pincer.close();
-//  	delay(1500);
-//  	}
-//    Serial.println("Testing distanceSensor...");
-  
-  
-//  	 for (int c = 0; c < 5000; c++) {
-//  	 char str[50];
-//  	 sprintf(str, "distanceSensor: distance= %d", distanceSensor.get_distance());
-//  	 Serial.println(str);
-//  	 delay(1);
-//  	}
-//        Serial.println("Testing display...");
-//  
-//  	display.clear();
-//  	display.drawCircle(3,3, 3);
-//        Serial.println("Testing buzzer...");
-//  
-//  	buzzer.playNote(NOTE_A4,300);
-//  	delay(300);
-//  	buzzer.playNote(NOTE_B4,300);
-//  	delay(300);
-//  	buzzer.playNote(NOTE_C4,300);
-//  	delay(300);
-//        Serial.println("Testing drive...");
- 
-//  drive.spinLeft();
-//  delay(1000);
-//  drive.spinRight();
-//  delay(1000);
-//        Serial.println("Testing button...");
-//  
-//  
-//  	for (int c = 0; c < 500; c++) {
-//  	     char str[50];
-//  	     sprintf(str, "button: pressed = %s", button.isPressed() ? "yes" : "no");
-//  	     Serial.println(str);
-//  	     delay(1);
-//  	}
-//        Serial.println("Testing bump...");
-//  
-//  
-//  	for (int c = 0; c < 500; c++) {
-//  	     char str[50];
-//  	     sprintf(str, "bump: pressed = %s", bump.isPressed() ? "yes" : "no");
-//  	     Serial.println(str);
-//  	     delay(1);
-//  	}
-//        Serial.println("Testing bump_3...");
-//  
-//  
-//  	for (int c = 0; c < 500; c++) {
-//  	     char str[50];
-//  	     sprintf(str, "bump_3: pressed = %s", bump_3.isPressed() ? "yes" : "no");
-//  	     Serial.println(str);
-//  	     delay(1);
-//  	}
-        
+
 }
-
-
